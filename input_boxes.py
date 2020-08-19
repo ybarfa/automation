77011dd04a7b1332b18a610842e67b478e4a146a
barfa@yash123 <barfayash2@gmail.com>
2020-08-19 23:18:28 +0530

    emprovised

--- input_boxes.py
+++ input_boxes.py
@@ -10,6 +10,8 @@ driver.get('https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407')
 inputboxes=driver.find_elements(By.CLASS_NAME,'text_field')
 print(len(inputboxes))
 
+# How to get the status
+
 status=driver.find_element(By.ID,'RESULT_TextField-1').is_displayed()
 print("Displayed or not:", status)  # gives either True/False
 
@@ -24,5 +26,5 @@ driver.find_element(By.ID,'RESULT_TextField-1').send_keys("yash")
 driver.find_element(By.ID,'RESULT_TextField-2').send_keys("barfa")
 driver.find_element_by_id('RESULT_TextField-3').send_keys("1234567890")
 
-# How to get the status
+
 
