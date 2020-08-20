a11b64c74a3dfe2c3df7d16ee154f93133415358
barfa@yash123 <barfayash2@gmail.com>
2020-08-20 21:13:47 +0530

    added more class

--- class_And_objects.py
+++ class_And_objects.py
@@ -30,3 +30,22 @@ t1=Test()
 t2=Test()
 t3=Test()
 t1.m1()
+
+
+
+
+class Test:
+    def __init__(self):
+        self.a=10
+        self.b=20
+        self.c=30
+        self.d=40
+    def m1(self):
+        del self.d
+
+t=Test()
+print(t.__dict__)
+t.m1()
+print(t.__dict__)
+del t.c
+print(t.__dict__)
