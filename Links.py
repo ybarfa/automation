9d7e322f148bc980a2310c9e20f79e00d05ca994
barfa@yash123 <barfayash2@gmail.com>
2020-08-21 20:34:43 +0530

    links present in a page

---
+++ Links.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome(executable_path="D:\\softwares\\gekodriver/\chromedriver.exe")
+
+driver.get('https://www.facebook.com/')
+
+links=driver.find_elements(By.TAG_NAME,"a")
+
+print("Number of links present:",len(links)) # Print how many links present in a page
+
+for link in links:
+    print(link.text)
+
+# Clicking on the link
+
+#driver.find_element(By.PARTIAL_LINK_TEXT,"Developers").click()
+
+driver.find_element(By.LINK_TEXT,"Developers").click()
