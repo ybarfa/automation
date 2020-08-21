e8efa36666eff952a7d21a43f4cd983c0cc97837
barfa@yash123 <barfayash2@gmail.com>
2020-08-21 15:12:42 +0530

    added a new file

---
+++ dropdown.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+
+driver=webdriver.Chrome(executable_path="D:\\softwares\\gekodriver/\chromedriver.exe")
+
+driver.get('https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407')
+
+element=(driver.find_element_by_id('RESULT_RadioButton-9'))
+drp=Select(element)
+
+# select by visible text
+#drp.select_by_visible_text('Morning') #Morning
+
+# select by index
+#drp.select_by_index(2) #Afternoon
+
+#Select by value
+drp.select_by_value('Radio-2') #Evening
+
+
+#Count number of options
+
+print(len(drp.options))
+
+#Capture all the options and print them as output
+
+all_options=drp.options
+
+for option in all_options:
+    print(option.text)
