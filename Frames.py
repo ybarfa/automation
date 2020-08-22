c665032fdaf3b2343276b708cc839339c5f1fc96
barfa@yash123 <barfayash2@gmail.com>
2020-08-22 21:34:21 +0530

    How to change frames in a page

---
+++ Frames.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+driver=webdriver.Chrome(executable_path="D:\\softwares\\gekodriver/\chromedriver.exe")
+
+driver.get('https://www.selenium.dev/selenium/docs/api/java/index.html')
+
+driver.switch_to.frame('packageListFrame') #First frame
+driver.find_element_by_link_text('org.openqa.selenium').click()
+time.sleep(3)
+
+driver.switch_to.default_content()
+
+driver.switch_to.frame('packageFrame') #Second frame
+driver.find_element_by_link_text('WebDriver').click()
+time.sleep(3)
+driver.switch_to.default_content()
+time.sleep(3)
+driver.switch_to.frame('classFrame') #Third frame
+driver.find_element_by_xpath('/html/body/div[1]/ul/li[5]').click()
