19b0e8bfd69a5fcf8ceaed7571c23cc24c8fdbc9
barfa@yash123 <barfayash2@gmail.com>
2020-08-24 21:01:14 +0530

    command line argument

--- command_line_argument.py
+++ command_line_argument.py
@@ -1,11 +1,11 @@
 # COMMAND LINE ARGUMENT #
 
-#from sys import argv
-#print("the number of command line argument:",len(argv))
-#print("the list of command line argument:",argv)
-#print("command line argument one by one:")
-#for x in argv:
-#    print(x)
+from sys import argv
+print("the number of command line argument:",len(argv))
+print("the list of command line argument:",argv)
+print("command line argument one by one:")
+for x in argv:
+    print(x)
 
 #------------------------------------------------------------#
 
