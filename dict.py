38d9de1be25fd4fdfd7e27ea4e9876c428ea59cb
barfa@yash123 <barfayash2@gmail.com>
2020-08-25 16:20:18 +0530

    if we want to represent a group of objects as key-value pairs

---
+++ dict.py
@@ -0,0 +1,11 @@
+employee1={"emp_name":"yash","emp_age":20,"emp_salary":10000}
+print(employee1)
+print(employee1["emp_name"])
+print(employee1["emp_age"])
+print(employee1["emp_salary"])
+
+employee2={"emp_name":"gaurav","emp_age":17,"emp_salary":17000}
+
+company={"employee1 details": employee1,"employee2 details":employee2}
+
+print(company)
