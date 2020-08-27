dd02031b790b0ae3a153ed9d8585f2a4c1be1478
barfa@yash123 <barfayash2@gmail.com>
2020-08-27 16:33:58 +0530

    restructured and create a new directory

--- mongo/jsan.py
+++ mongo/jsan.py
@@ -6,3 +6,8 @@ my_many_records_1=[{"name":"yash","rollno":1,"subject":"maths","marks":88}]
 x=mycollection.insert_many(my_many_records_1)
 my_many_records_2=[{"name":"jay","rollno":2,"subject":"science","marks":89}]
 x=mycollection.insert_many(my_many_records_2)
+my_many_records_3=[{"name":"aman","rollno":3,"subject":"hindi","marks":78}]
+x=mycollection.insert_many(my_many_records_3)
+
+x = mycollection.find_one()
+print(x)
---
+++ practice/dict.py
@@ -0,0 +1,12 @@
+employee1={"emp_name_1":"yash","emp_age_1":20,"emp_salary_1":10000}
+print(employee1.keys())
+print(employee1.values())
+
+
+employee2={"emp_name_2":"gaurav","emp_age_2":17,"emp_salary_2":17000}
+print(employee2.keys())
+print(employee2.values())
+
+
+company={"employee_1 details": employee1,"employee_2 details":employee2}
+print(company)
