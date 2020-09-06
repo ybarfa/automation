46d6840c933639f6f16f835c3705207f0d059c99
barfa@yash123 <barfayash2@gmail.com>
2020-09-06 20:14:58 +0530

    working on python 3 keywords

---
+++ practice/Break.py
@@ -0,0 +1,20 @@
+
+
+
+av=5
+
+
+x=int(input("How many candies you want?"))
+
+i=1
+while i<= x:
+
+    if i>av:
+        print("out of stock")
+        break
+
+
+    print("candy")
+    i+=1
+
+print("Bye")
