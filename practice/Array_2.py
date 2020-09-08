34770771b284926f50f3ee6adce498788c2cbdef
barfa@yash123 <barfayash2@gmail.com>
2020-09-08 17:36:04 +0530

    Accepting values from user and store them in array

---
+++ practice/Array_2.py
@@ -0,0 +1,23 @@
+from array import *
+
+arr = array('i',[])
+
+n = int(input("Enter The Length Of The Array"))
+
+for i in range(n):
+    x = int(input("Enter The Next Value"))
+    arr.append(x)
+
+
+print(arr)
+
+val = int(input("Enter The Value For Search"))
+
+k = 0
+for e in arr:
+    if e==val:
+        print(k)
+        break
+    k+=1
+
+print(arr.index(val))
