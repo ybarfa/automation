9e3df83085d6c54b6d5d77f9c2cd30beb7f1691d
barfa@yash123 <barfayash2@gmail.com>
2020-09-10 19:13:39 +0530

    different ways of creating array using numpy

---
+++ practice/array_using_numpy.py
@@ -0,0 +1,42 @@
+# Using array itself #
+
+from numpy import *
+
+arr=array([1,2,3,4,5])
+print(arr.dtype)
+print(arr)
+
+# Using linspace() #
+
+from numpy import *
+
+arr=linspace(0,15)
+print(arr)
+
+# Using arange() #
+
+from numpy import *
+
+arr=arange(1,15,2)
+print(arr)
+
+# Using logspace() #
+
+from numpy import *
+
+arr=logspace(1,40,5)
+print(arr)
+
+# Using zeros #
+
+from numpy import *
+
+arr=zeros(5)
+print(arr)
+
+# Using ones #
+
+from numpy import *
+
+arr=ones(5)
+print(arr)
