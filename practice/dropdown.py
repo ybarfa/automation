fb49feecab3e7dfced877192fdf460e8453b5147
barfa@yash123 <barfayash2@gmail.com>
2020-09-11 22:14:09 +0530

    restructured and create a new directory

--- practice/dropdown.py
+++ practice/dropdown.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-
+import time
 
 driver=webdriver.Chrome(executable_path="D:\\softwares\\gekodriver/\chromedriver.exe")
 
@@ -10,14 +10,19 @@ element=(driver.find_element_by_id('RESULT_RadioButton-9'))
 drp=Select(element)
 
 # select by visible text
-#drp.select_by_visible_text('Morning') #Morning
+drp.select_by_visible_text('Morning') #Morning
+
+time.sleep(5)
 
 # select by index
-#drp.select_by_index(2) #Afternoon
+drp.select_by_index(2) #Afternoon
+
+time.sleep(5)
 
 #Select by value
 drp.select_by_value('Radio-2') #Evening
 
+time.sleep(5)
 
 #Count number of options
 
@@ -29,3 +34,5 @@ all_options=drp.options
 
 for option in all_options:
     print(option.text)
+
+driver.quit()
