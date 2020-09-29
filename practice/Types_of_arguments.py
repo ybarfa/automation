9df75c39a66f64634e25b652f443c2f92e935c62
barfa@yash123 <barfayash2@gmail.com>
2020-09-29 17:19:09 +0530

    Types of arguments in python

---
+++ practice/Types_of_arguments.py
@@ -0,0 +1,28 @@
+# Position Argument #
+
+def person(name,age):
+    print(name+' Barfa ')
+    print(age-5)
+
+person(age=20,name='Yash')
+
+# Default Argument #
+
+def person(name,age=18):
+    print(name+' Barfa ')
+    print(age)
+
+person('Yash')
+
+# Variable Length Argument #
+
+def sum(a, *b):
+
+    c=a
+
+    for i in b:
+        c=c+i
+
+    print(c)
+
+sum(5,6,34,78,89)
